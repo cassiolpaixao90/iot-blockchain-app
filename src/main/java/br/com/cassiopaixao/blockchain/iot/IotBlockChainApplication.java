720f3eed943225f9a1a6af930846f8a768a103b5
Cassio Paixao <cassiopaixaoti@gmail.com>
2018-08-12 13:56:28 -0300

    Initial commit

---
+++ src/main/java/br/com/cassiopaixao/blockchain/iot/IotBlockChainApplication.java
@@ -0,0 +1,12 @@
+package br.com.cassiopaixao.blockchain.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotBlockChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotBlockChainApplication.class, args);
+	}
+}
