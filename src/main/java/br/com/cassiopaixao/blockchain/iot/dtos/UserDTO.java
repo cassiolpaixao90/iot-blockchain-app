56127b224a30e0e67eccbf0ffb0ffc93f4eb5272
Cassio Paixao <cassiopaixaoti@gmail.com>
2018-08-12 15:04:33 -0300

    security and exception

--- src/main/java/br/com/cassiopaixao/blockchain/iot/dtos/UserDTO.java
+++ src/main/java/br/com/cassiopaixao/blockchain/iot/dtos/UserDTO.java
@@ -4,9 +4,11 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
 
 import br.com.cassiopaixao.blockchain.iot.models.Users;
 
+@Component
 public class UserDTO extends User {
 
 	private static final long serialVersionUID = 1L;
